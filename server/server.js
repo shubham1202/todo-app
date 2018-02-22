b268c52c4bb68d5a6015b9fad2d6c10a7d185d9e
Shubham Goel <shubham.developer95@gmail.com>
2018-02-22 16:14:57 +0530

    List all todos

--- server/server.js
+++ server/server.js
@@ -29,6 +29,15 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+//Get listing of all todos
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
 //Server setup(Listen to port)
 app.listen(3000, ()=> {
 	console.log('Started on port 3000');
