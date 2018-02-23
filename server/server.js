11197539063f2f98dbd4c424fc3e9ae07bc2b0eb
Shubham Goel <shubham.developer95@gmail.com>
2018-02-23 11:16:05 +0530

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //library imports
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 //local imports
 var {mongoose} = require('./db/mongoose');
@@ -10,7 +11,7 @@ var {User}     = require('./models/user');
 //Server setup(include express in our application)
 var app = express();
 
-//middleware(bodyParser Json data and returns function(Javascript object data))
+//middleware(bodyParser takes Json data and returns function(Javascript object data))
 app.use(bodyParser.json());
 
 //Create a resource(new todo)
@@ -35,6 +36,24 @@ app.get('/todos', (req, res) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	//res.send(req.params);
+	var id = req.params.id;
+	
+	if(!ObjectId.isValid(id)) {
+		return res.status(404).send();
+	}
+	
+Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(400).send();
+		}
+		res.send(JSON.stringify({todo}, undefined, 2));
+	}).catch((e) => {
+		res.status(404).send();
 	});
 });
 
